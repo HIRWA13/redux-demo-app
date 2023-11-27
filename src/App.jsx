b62039c4eac1be223a7beca734f3c41ef4f0b196
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-27 11:40:37 +0200

    feat: update the app root component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import CakeContainer from "./components/CakeContainer"
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <CakeContainer />
+      </div>
+    </Provider>
+  );
+}
+export default App
