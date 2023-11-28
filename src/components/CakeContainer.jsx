0c3979ee97b4ff61bb44e17b9f92b97b55858077
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-28 11:36:15 +0200

    feat: update the cake container

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -2,10 +2,19 @@ import { connect } from "react-redux";
 import { buyCake } from "../redux";
 
 function CakeContainer(props) {
+    const validator = () => {
+        if(props.numberOfCakes < 1) {
+            alert ("no more cakes in the store")
+        }
+    }
+    const buyCakeHandler = () => {
+        props.buyCake()
+        validator()
+    }
   return (
     <div>
       <h2>Number of cakes: {props.numberOfCakes}</h2>
-      <button onClick={props.buyCake}>Buy Cake</button>
+      <button onClick={buyCakeHandler}>Buy Cake</button>
     </div>
   );
 }
